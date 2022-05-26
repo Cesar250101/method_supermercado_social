988f48bdebba066fd9fcf542b5c95dbb799d8d78
Cesar250101 <cesar250101@outlook.com>
2022-05-25 21:22:14 -0400

    Validar salidas de inventario uno a uno

--- models/models.py
+++ models/models.py
@@ -42,6 +42,36 @@ class OdenesPos(models.Model):
 class ModuleName(models.Model):
     _inherit = 'stock.picking'
 
+
+    @api.one
+    def valida_entrega_uno(self):
+        move=self.env['stock.move'].search([('picking_id','=',self.id)])
+        for m in move:
+            move_line=self.env['stock.move.line'].search([('move_id','=',m.id)])
+            if not move_line:
+                domain = [
+                            ("product_id", "=", m.product_id.id),
+                            ("quantity", ">", 0),
+                            ('location_id','=',self.location_id.id)
+                        ]
+                lote=self.env['stock.quant'].search(domain,order="in_date",limit=1)                
+                vals={
+                        'picking_id':self.id,
+                        'move_id':m.id,
+                        'product_id':m.product_id.id,
+                        'product_uom_id':m.product_uom.id,
+                        #'product_uom_qty':m.product_uom_qty,
+                        #'product_qty':m.product_uom_qty,
+                        'qty_done':m.product_uom_qty,
+                        'location_id':m.location_id.id,
+                        'location_dest_id':m.location_dest_id.id,
+                        'lot_id':lote.lot_id.id,
+                    }
+                move_line.sudo().create(vals)
+                move_line=self.env['stock.move.line'].search([('move_id','=',m.id)])
+        self.button_validate()
+
+
     @api.model
     def valida_entrega(self):
         pendientes=self.search([("state","in",['assigned','confirmed']),('picking_type_id','=',2)],limit=100)
