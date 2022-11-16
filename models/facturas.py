c058f38cbd85fde1054ceab5599b84bdae23232a
Cesar250101 <cesar250101@outlook.com>
2022-11-16 10:29:36 -0300

    Conciliación NC con Factura

--- models/facturas.py
+++ models/facturas.py
@@ -5,7 +5,7 @@ import xlrd
 import io
 from odoo.tools import pycompat
 from datetime import datetime
-
+from odoo.tools.safe_eval import safe_eval
 
 class Payment_wizard(models.TransientModel):
     _name = 'invoice.wizard'
@@ -126,3 +126,59 @@ class Payment_wizard(models.TransientModel):
                     'amount_total_signed':(factura.amount_total_signed)*-1,
                     'amount_total_company_signed':(factura.amount_total_company_signed)*-1,
                     })
+                    conciliar=self.conciliar_nc(mode='cancel',factura_id=factura,nota_credito=nota_credito)
+                    print(conciliar)
+
+    @api.multi
+    def conciliar_nc(self, mode='refund',factura_id=False,nota_credito=False):
+        inv_obj = self.env['account.invoice']
+        inv_tax_obj = self.env['account.invoice.tax']
+        inv_line_obj = self.env['account.invoice.line']
+        context = dict(self._context or {})
+        xml_id = False
+        invoice_refund=self.env['account.invoice.refund']
+        for form in self:
+            created_inv = []
+            date = False
+            description = False
+            refund = nota_credito
+            created_inv.append(refund.id)
+            if mode in ('cancel', 'modify'):
+                movelines = factura_id.move_id.line_ids
+                to_reconcile_ids = {}
+                to_reconcile_lines = self.env['account.move.line']
+                for line in movelines:
+                    if line.account_id.id == factura_id.account_id.id:
+                        to_reconcile_lines += line
+                        to_reconcile_ids.setdefault(line.account_id.id, []).append(line.id)
+                    if line.reconciled:
+                        line.remove_move_reconcile()
+                refund.action_invoice_open()
+                for tmpline in refund.move_id.line_ids:
+                    if tmpline.account_id.id == factura_id.account_id.id:
+                        to_reconcile_lines += tmpline
+                to_reconcile_lines.filtered(lambda l: l.reconciled == False).reconcile()
+                xml_id = factura_id.type == 'out_invoice' and 'action_invoice_out_refund' or \
+                         factura_id.type == 'out_refund' and 'action_invoice_tree1' or \
+                         factura_id.type == 'in_invoice' and 'action_invoice_in_refund' or \
+                         factura_id.type == 'in_refund' and 'action_invoice_tree2'
+        if xml_id:
+            result = self.env.ref('account.%s' % (xml_id)).read()[0]
+            if mode == 'modify':
+                # When refund method is `modify` then it will directly open the new draft bill/invoice in form view
+                if inv_refund.type == 'in_invoice':
+                    view_ref = self.env.ref('account.invoice_supplier_form')
+                else:
+                    view_ref = self.env.ref('account.invoice_form')
+                form_view = [(view_ref.id, 'form')]
+                if 'views' in result:
+                    result['views'] = form_view + [(state,view) for state,view in result['views'] if view != 'form']
+                else:
+                    result['views'] = form_view
+                result['res_id'] = inv_refund.id
+            else:
+                invoice_domain = safe_eval(result['domain'])
+                invoice_domain.append(('id', 'in', created_inv))
+                result['domain'] = invoice_domain
+            return result
+        return True
